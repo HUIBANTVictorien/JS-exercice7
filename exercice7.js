1816ddebc2a70f3a2b4460eb3bd7fe7c5dd00636
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-10-24 12:28:46 +0200

    changement code

--- exercice7.js
+++ exercice7.js
@@ -1,6 +1,6 @@
-function controle(form) {
-  var pointure = document.form.pointure.value;
-  var annee = document.form.annee.value;
+function display() {
+  var pointure = document.getElementById('pointure').value;
+  var annee = document.getElementById('annee').value;
   result = (pointure * 2 + 5) * 50 - annee + 1766;
   alert(result);
 };
